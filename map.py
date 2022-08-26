851afd052f67dc77cff8d83741ee68cb44e65a3b
antdon <a.donohue@uqconnect.edu.au>
2022-08-27 06:22:18 +1000

    addedd more things in initial map

--- map.py
+++ map.py
@@ -14,9 +14,19 @@ class Map():
         self.grid : dict[Tile] = {(y,x) : Tile((y,x)) 
             for x,y in itertools.product(range(80), repeat=2)}
 
+
         for i in range(20,29):
             for j in range(47,57):
                 self.grid[(i, j)].content = Tree((i,j))
+
+        for i in range(0, 11):
+            for j in range(4,13):
+                self.grid[(i, j)].content = Berry((i,j))
+
+        for i in range(35, 40):
+            for j in range(24, 30):
+                self.grind[(i, j)].content = Vein((i,j))
+
 
 
 
