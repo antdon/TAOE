b7113b36ec021132b4a7efb8f55a63597c6941ac
rowboat1 <asteroixiii@gmail.com>
2022-08-27 10:51:12 +1000

    Unit collection rate

--- unit.py
+++ unit.py
@@ -3,6 +3,10 @@ class Unit():
         self.location = location
         self.health = health
 
+    def draw(self, stdscr):
+        
+        pass
+
 class Villager(Unit):
     def __init__(self, location, health: int, capacity:int) -> None:
         super().__init__(location, health)
@@ -11,6 +15,12 @@ class Villager(Unit):
         self.gold: int = 0
         self.wood: int = 0
         self.stone: int = 0
+        self.time_on_task: int = 0
+        self.gather_rate: int = 500
+
+    def gather(self, target, delta_time):
+        self.time_on_task += delta_time
+        pass
 
 class Soldier(Unit):
     def __init__(self, location, health: int, level: int) -> None:
